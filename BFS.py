41f9be554e83060eadba858eb55f6955b1d2f804
Yu Zhengwen <yuzhengwen0217@gmail.com>
2024-04-12 01:43:11 +0800

    Added 6 Other algorithms and updated README to explain the different algorithm implementations and results

---
+++ BFS.py
@@ -0,0 +1,164 @@
+import copy
+
+# ------------------------CLASS DEFINITIONS------------------------
+class PuzzleGrid:
+    def __init__(self, length):
+        self.length = length
+        self.grid = [[' ' for _ in range(length)] for _ in range(length)]
+    def set_grid(self, grid):
+        self.grid = copy.deepcopy(grid)
+
+    def display(self):
+        for row in self.grid:
+            print(' | '.join(row))
+            print('-' * (self.length * 4 - 1))
+
+    def set_cell(self, row, col, value):
+        if 0 <= row < self.length and 0 <= col < self.length:
+            self.grid[row][col] = value
+        else:
+            print("Invalid row or column index")
+
+    def get_cell(self, row, col):
+        if 0 <= row < self.length and 0 <= col < self.length:
+            return self.grid[row][col]
+        else:
+            #print("Invalid row or column index")
+            return None
+    def get_cell_pos(self, value):
+        for i in range(self.length):
+            for j in range(self.length):
+                if self.grid[i][j] == value:
+                    return (i, j)
+        return None
+    def swap_cells(self, row1, col1, row2, col2):
+        temp = self.grid[row1][col1]
+        self.grid[row1][col1] = self.grid[row2][col2]
+        self.grid[row2][col2] = temp
+    def swap_values(self, value1, value2):
+        pos1 = self.get_cell_pos(value1)
+        pos2 = self.get_cell_pos(value2)
+        if pos1 and pos2:
+            self.swap_cells(pos1[0], pos1[1], pos2[0], pos2[1])
+        else:
+            print("Invalid cell value")
+    def clone(self):
+        new_puzzle = PuzzleGrid(self.length)
+        new_puzzle.set_grid(self.grid)
+        return new_puzzle
+    def get_diff_count(self, other):
+        count = 0
+        for i in range(self.length):
+            for j in range(self.length):
+                if self.grid[i][j] != other.grid[i][j]:
+                    count += 1
+        return count
+    def is_equal(self, other):
+        return self.get_diff_count(other) == 0
+    def get_adjacent_values(self, value):
+        cell_pos = self.get_cell_pos(value)
+        adjacent_cells = [(cell_pos[0] + 1, cell_pos[1]), (cell_pos[0] - 1, cell_pos[1]), (cell_pos[0], cell_pos[1] + 1), (cell_pos[0], cell_pos[1] - 1)]
+        adjacent_cell_values = []
+        for cell in adjacent_cells:
+            if self.get_cell(cell[0], cell[1]):
+                adjacent_cell_values.append(self.get_cell(cell[0], cell[1]))
+        return adjacent_cell_values
+    def get_possible_states(self):
+        possible_states = []
+        adjacent_cell_values = self.get_adjacent_values('0')
+        for value in adjacent_cell_values:
+            new_puzzle = self.clone()
+            new_puzzle.swap_values('0', value)
+            possible_states.append(new_puzzle)
+        return possible_states
+    
+class Node:
+    def __init__(self, puzzle_state, parent=None):
+        self.puzzle_state = puzzle_state
+        self.parent = parent
+        if parent:
+            self.depth = parent.depth + 1
+        else:
+            self.depth = 0
+    def expand(self):
+        nodes = []
+        possible_states = self.puzzle_state.get_possible_states()
+        for state in possible_states:
+            if str(state.grid) in visited:
+                continue
+            nodes.append(Node(state, self))
+        return nodes
+
+# ------------------------END CLASS DEFINITIONS------------------------
+
+default_goal4 = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '0']]
+default_goal3 = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']]
+
+def get_input_grid():
+    inputStr = input()
+    # return default goal states
+    if (inputStr == "3x3"):
+        return default_goal3
+    if (inputStr == "4x4"):
+        return default_goal4
+    
+    inputs = inputStr.split(' ')
+    # check input has correct number of elements
+    if len(inputs) != grid_length**2:
+        print("Invalid grid!")
+        return get_input_grid()
+    # create grid from input
+    grid = [[' ' for _ in range(grid_length)] for _ in range(grid_length)]
+    for i in range(grid_length):
+        for j in range(grid_length):
+            grid[i][j] = inputs[i * grid_length + j]
+    return grid
+
+visited = set()
+frontier_nodes = []
+def solve(puzzle):
+    frontier_nodes.append(Node(puzzle))
+    
+    while frontier_nodes:
+        node = frontier_nodes.pop(0)
+        print("")
+        print("-----CURRENT NODE-----")
+        print("Depth:", node.depth)
+        #node.puzzle_state.display()
+        if node.puzzle_state.is_equal(goal_puzzle):
+            print("Puzzle solved!")
+            print("PATH TO SOLUTION:")
+            print_path(node)
+            return
+        visited.add(str(node.puzzle_state.grid))
+        print("-----EXPANDING NODE-----")
+        children = node.expand() 
+        frontier_nodes.extend(children)
+        #print([str(x) for x in frontier_nodes])
+        print("Frontier nodes:", len(frontier_nodes))
+        print("Visited nodes:", len(visited))
+
+step_count = 0
+def print_path(node):
+    global step_count
+    if node.parent:
+        print_path(node.parent)
+        step_count += 1
+    node.puzzle_state.display()
+    print()
+
+# ------------------------MAIN CODE------------------------
+grid_length = int(input("Enter the grid length: (3 for 3x3, 4 for 4x4, etc.)"))
+puzzle = PuzzleGrid(grid_length)
+goal_puzzle = PuzzleGrid(grid_length)
+
+print("Enter the initial grid:")
+puzzle.set_grid(get_input_grid())
+puzzle.display()
+
+print("Enter the goal grid:")
+goal_state = get_input_grid()
+goal_puzzle.set_grid(goal_state)
+
+solve(puzzle)
+print("Steps:", step_count)
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To tackle this problem, we use a non-autonomous utility-based agent. The agent i
 ---
 # How to Use
 1) Open a command prompt/terminal window in the same directory as the py file
-2) Type `py main.py` and enter
+2) Type `py main.py` and enter (This will run the A* implementation, you can run the other algorithms too. Process is similar)
 3) It will ask for 3 inputs
     - Grid length: '3' represents a 3x3 grid, etc.
     - Initial position: Make sure to give the correct number of elements (denoted by grid length), separated by space. '0' represents a blank space. e.g. `1 2 3 4 0 5 6 7 8`
@@ -18,7 +18,7 @@ To tackle this problem, we use a non-autonomous utility-based agent. The agent i
 4) (Hopefully) After performing a search, it should print out the step by step solution to solve the puzzle!
 
 ---
-# Solution Explanation
+# Solution Explanation (Main: A*)
 ## Python Classes Used
 ### Puzzle Grid Object
 Contains the puzzle grid which is a 2-d array of [m][m].
@@ -60,3 +60,38 @@ We only need to pass in the initial and final grid into the function.
 ## Additional Considerations
 The total number of possible ways to arrange numbers 0-8 on 9 grids is 9!=362880. It is worth noting that from any given state, **not all** 362879 other states may be reachable. So the possible states may be a significantly reduced number, but nevertheless relatively large.   
 Our algorithm **will not check a same state twice** for efficiency (and to prevent agent from looping back n forth between 2 states infinitely), however worst case scenarios there still may be tens of thousands of states to check through, while some setups may be inherently unsolveable!
+
+# Other Algorithms
+After much testing, we A* seems to be the most consistent in finding the shortest path in shortest time. But 6 other algorithms have been implemented as well and are labelled accordingly.
+
+## Algorithms that do not use a heuristic function
+### Breadth First Search
+BFS will search every node in each depth before expanding.   
+E.g. It will expand every depth=1 node and check for goal state, if not it will expand every depth=2 node, and this repeats until goal state is found.  
+This will be able to find the shortest distance to solution, but will end up expanding many unnecesary nodes and the search time gets exponentially longer.
+
+### Depth First Search
+We have 3 variations of DFS: The basic DFS, depth-limited, and iterative-deepening:  
+The ***basic DFS*** is highly unsuitable for our problem and the result is completely luck based. If the first node it chooses to expand contains the goal puzzle, then it will find the solution (often not optimal), but if that node does not have the goal state, then it will forever continuously expand down one branch.
+***Depth Limited*** only works if the solution is within the depth the user inputs. Even if goal state is reachable within specified depth, due to randomly exploring each branch to the end, the solution may not be optimal as well.
+Lastly ***Iterative-Deepening*** will accept a max depth limit from user (can be a large number), then it will perform Depth Limited search with increasing depth limit `1 < limit < max depth limit `. It suffers from the same drawback of possibly not providing optimal solution
+
+## Algorithms with alternate Heuristic Functions
+I have also implemented 2 other algorithms that use a heuristic function similar to A*, namely ***Uniform Cost Search (UCS)*** & ***Greedy Search***.  
+For UCS: `f = g(depth)`
+For Greedy: `f = h(No. of blocks in the wrong position or Difference from goal)`  
+Both have been proven to be less efficient than A* in finding optimal solution and searching the least amount of nodes.
+
+# Test Cases
+To observe the difference between the algorithms, we recommend working backwards from the goal state and using an initial state not more than 5 moves away from the goal state. Then you can analyse the log to see the difference in how the nodes are searched.  
+As of what we have tested, the **longest** solution A* Search could find was with the initial state `0 5 2 1 3 8 6 7 4`. It takes awhile to search but the solution was an unbelievable 22 steps!! (Feel free to try solving it manually :D)  
+
+ 0 | 5 | 2 
+ 1 | 3 | 8 
+ 6 | 7 | 4 
+
+
+
+Remember that 0 represents blank space!
+
+(Quite often) when the search takes too long you can `ctrl+c` to stop (if running in a terminal)!
